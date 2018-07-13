1a66b1db173be783dffd7e7a0f10df7c5ad14951
Jonata Hessa <jonata.hessa@gmail.com>
2018-07-13 17:54:08 -0300

    Parte 2 de diretivas, ng-show, ng-class e etc

--- AngularJS/dom.js
+++ AngularJS/dom.js
@@ -7,8 +7,31 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($s
     {nome: "Jonata", telefone: "99999-7777"},
     {nome: "Maria", telefone: "99999-6666"}
   ];
+
+  $scope.operadoras = [
+    {nome: "Oi", codigo: 14, categoria: "Celular"},
+    {nome: "Vivo", codigo: 15, categoria: "Celular"},
+    {nome: "Tim", codigo: 41, categoria: "Celular"},
+    {nome: "GVT", codigo: 25, categoria: "Fixo"},
+    {nome: "Embratel", codigo: 21, categoria: "Fixo"}
+  ];
+
   $scope.adicionarContato = function (contato) {
     $scope.contatos.push(angular.copy(contato));
     delete $scope.contato;
-  }
+  };
+
+  $scope.apagarContatos = function (contatos) {
+    $scope.contatos = contatos.filter(function (contato) {
+      if (!contato.selecionado) {
+        return contato;
+      }
+    });
+  };
+
+  $scope.isContatoSelecionado = function (contatos) {
+    return contatos.some(function (contato) {
+      return contato.selecionado;
+    });
+  };
 });
